edc391a29aefec8694fbe4eb7b142ea294fb2c26
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-07-10 13:43:30 +0530

    add gradient to qr sizes

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,27 @@
 const LOADING = Symbol('loading');
 const READY = Symbol('ready');
 
+const options = {
+  size: 500,
+  quality: {
+    low: 'L',
+    medium: 'M',
+    quartile: 'Q',
+    high: 'H',
+  },
+};
+
+const qrSizeRanges = [];
+for (let i = 0; i <= 500; i += 50) {
+  qrSizeRanges.push(i);
+}
+
+const MAX_LENGTH = 1500;
+const MIN_LENGTH = 0;
+
+const MAX_SIZE = 500;
+const MIN_SIZE = 50;
+
 function executeScript(code) {
   return new Promise((res, rej) => {
     try {
@@ -37,31 +58,34 @@ function toggleStatus(status) {
   try {
     toggleStatus(LOADING);
 
-    const options = {
-      size: 500,
-      quality: {
-        low: 'L',
-        medium: 'M',
-        quartile: 'Q',
-        high: 'H',
-      },
-    };
-
     const [location] = await executeScript('window.location.href');
     const [selection] = await executeScript('window.getSelection().toString()');
+
+    const text = selection.length > 0 ? selection : location;
+    if (text.length > MAX_LENGTH) {
+      throw Error('text.length > MAX_LENGTH');
+    }
+
+    const size = qrSizeRanges.find(
+      size => size > (
+        MIN_SIZE
+        +
+        (MAX_SIZE - MIN_SIZE) * Math.log1p(text.length) / Math.log1p(MAX_LENGTH)
+      )
+    );
 
     const query = {
       cht: 'qr',
       choe: 'UTF-8',
-      chs: `${options.size}x${options.size}`,
+      chs: size + 'x' + size,
       chld: options.quality.high,
-      chl: selection.length > 0 ? selection : location,
+      chl: text,
     };
 
     const queryString = Object.keys(query).map(k => `${k}=${query[k]}`).join('&');
 
     qr.onload = () => toggleStatus(READY);
-    qr.src = `https://chart.googleapis.com/chart?${queryString}`;
+    qr.src = 'https://chart.googleapis.com/chart?' + queryString;
   } catch (err) {
     document.body.innerHTML = '&times;';
     document.body.classList.add('error');
